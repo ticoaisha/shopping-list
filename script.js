6f8ea42cfc5f7052501419475bec9719dc34bc40
Aishe <ticoaisha@gmail.com>
2023-05-13 19:34:15 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+// Event Listeners
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+  //Validate ipnput
+
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
+
+  // create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+  
+
+  const button = createButton('remove-item btn-link text-red');
+
+  li.appendChild(button);
+
+//   console.log(li);
+
+//   console.log(button);
+
+itemList.appendChild(li);
+
+itemInput.value = '';
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+itemForm.addEventListener('submit', addItem);
