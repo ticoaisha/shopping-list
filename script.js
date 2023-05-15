6bd1f3dfdf7d113c8534de41572d959d2c6a5b03
Aishe <ticoaisha@gmail.com>
2023-05-15 00:27:52 -0400

    Filter items added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
-
-// Event Listeners
+const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
   e.preventDefault();
@@ -18,19 +18,21 @@ function addItem(e) {
   // create list item
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(newItem));
-  
 
   const button = createButton('remove-item btn-link text-red');
 
   li.appendChild(button);
 
-//   console.log(li);
+  //   console.log(li);
 
-//   console.log(button);
+  //   console.log(button);
 
-itemList.appendChild(li);
+  // add list item to the list
+  itemList.appendChild(li);
 
-itemInput.value = '';
+  checkUI();
+
+  itemInput.value = '';
 }
 
 function createButton(classes) {
@@ -48,4 +50,57 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm()) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+  checkUI();
+}
+
+function clearItems(e) {
+  // console.log('works');
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+  //   console.log(text);
+
+  items.forEach((item) => {
+    // console.log(item);
+    const itemName = item.firstChild.textContent.toLowerCase();
+    // console.log(itemName);
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li');
+
+  if (items.length === 0) {
+    clearButton.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearButton.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+}
+
+// Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
